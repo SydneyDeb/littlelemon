260052d90034bc4cb0325cf9e3b34c64279eeece
unknown <BT_5E7_27@BKTECH.LOCAL>
2025-06-09 09:17:15 -0400

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import bannerImg from "../images/restauranfood.jpg";
+import bannerImg from "../images/restaurantfood.jpg";
 
 const Header = () => {
   return (
